77a4a61dcab56c02fc8503dde57922fa9c128955
Ivan Tchernev <ivan.useful@gmail.com>
2018-02-26 09:44:54 +0000

    Added swipe to delete functionality to table view

--- ImageGallery/SelectGalleryTableViewController.swift
+++ ImageGallery/SelectGalleryTableViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SelectGalleryTableViewController: UITableViewController {
 
     var imageGalleries: [ImageGallery] = [imageConstants.mandelbrotGallery]
+    var recentlyDeletedGalleries: [ImageGallery] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +37,20 @@ class SelectGalleryTableViewController: UITableViewController {
     
     // MARK: - Navigation
     
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        switch identifier {
+        case "openGallerySegue":
+            if let cell = sender as? UITableViewCell {
+                if let indexPath = tableView.indexPath(for: cell) {
+                    return indexPath.section == 0
+                }
+            }
+        default:
+            break
+        }
+        return false
+    }
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
@@ -60,11 +75,18 @@ class SelectGalleryTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imageGalleries.count
+        switch section {
+        case 0:
+            return imageGalleries.count
+        case 1:
+            return recentlyDeletedGalleries.count
+        default:
+            return 0
+        }
     }
 
     
@@ -72,10 +94,27 @@ class SelectGalleryTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "galleryRow", for: indexPath)
 
         // Configure the cell...
-        cell.textLabel?.text = imageGalleries[indexPath.row].name
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = imageGalleries[indexPath.row].name
+        case 1:
+            cell.textLabel?.text = recentlyDeletedGalleries[indexPath.row].name
+        default:
+            break
+        }
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Image Galleries"
+        case 1:
+            return "Recently Deleted"
+        default:
+            return nil
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -88,9 +127,24 @@ class SelectGalleryTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            imageGalleries.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            switch indexPath.section {
+            case 0:
+                // Delete the row from the data source
+                let deletedGallery = imageGalleries[indexPath.row]
+                tableView.performBatchUpdates({
+                    let destinationIndexPath = recentlyDeletedGalleries.count
+                    imageGalleries.remove(at: indexPath.row)
+                    recentlyDeletedGalleries.append(deletedGallery)
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                    tableView.insertRows(at: [IndexPath(row: destinationIndexPath, section: 1) ], with: .fade)
+                })
+            case 1:
+                // Delete the row from the data source
+                recentlyDeletedGalleries.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            default:
+                break
+            }
         } //else if editingStyle == .insert {
 //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
 //        }
